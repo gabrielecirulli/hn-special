8c49978284172f73ba5373bacfe43ccfb45d9d05
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-16 14:05:36 +0200

    code changes and comment with info

--- clear_styles.js
+++ clear_styles.js
@@ -1,23 +1,27 @@
-var $ = function(selector){
+// TODO: Add option to make links target _blank
+
+var $ = function (selector) {
     return [].slice.call(document.querySelectorAll(selector));
 };
 
 // Removes the original HN CSS to avoid conflicts with the CSS added by the extension 
-$("link[rel=stylesheet]").forEach(function(elem){
+$("link[rel=stylesheet]").forEach(function (elem) {
     elem.remove();
 });
  
 // Removes all styling attributes
-$("table, tr, td").forEach(function(elem){
+$("table, tr, td").forEach(function (elem) {
     var attrs = elem.attributes;
     var names = [];
 
-    for(var i = 0; i < attrs.length; i++) {
-      var attr = attrs[i];
-      names.push(attr.name);
+    // This is contrived because .length is changed, messing up the loop
+    for (var i = 0; i < attrs.length; i++) {
+      var attr = attrs[i].name;
+      if (attr === "colspan") continue; 
+      names.push(attr);
     }
 
-    names.forEach(function(attr){
+    names.forEach(function (attr) {
       elem.removeAttribute(attr);
     });
 });

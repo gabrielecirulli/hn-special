d4444c3b9ad1d4ee5a96513a976983c36492ad6e
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-16 11:59:21 +0200

    script to remove the original HN css

--- clear_styles.js
+++ clear_styles.js
@@ -1,1 +1,9 @@
-alert('hello')
+// Removes the original HN CSS to avoid conflicts with the CSS added by the extension
+var links = document.getElementsByTagName('link');
+
+for (var i = 0; i < links.length; i++) {
+  var link = links[i];
+  if (link.getAttribute("rel") === "stylesheet") {
+    link.parentNode.removeChild(link);
+  }
+}

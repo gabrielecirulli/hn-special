03716a091c87e4db576b30c9a9474b4abd8b052c
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-18 22:53:08 +0200

    create basic settings icon

--- settings.js
+++ settings.js
@@ -1,1 +1,15 @@
 // Settings management
+load(function () {
+  createMenu();
+  
+});
+
+function createMenu() {
+  // Replace the menu container with a div
+  var container = replaceTag($(".pagetop")[1], "div");
+
+  var button = document.createElement("div");
+  button.classList.add("hnspecial-settings-button");
+
+  container.insertBefore(button, container.children[0]);
+}
--- utility.js
+++ utility.js
@@ -9,3 +9,17 @@ function $(selector) {
 function load(callback) {
   document.addEventListener("DOMContentLoaded", callback);
 }
+
+function replaceTag(container, tag) {
+  var parent = container.parentElement;
+  var newContainer = document.createElement(tag);
+  toArray(container.classList).forEach(function (name) {
+    newContainer.classList.add(name);
+  });
+  while (container.firstChild) {
+    newContainer.appendChild(container.firstChild);
+  }
+  parent.replaceChild(newContainer, container);
+  
+  return newContainer;
+}

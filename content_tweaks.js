83b70af3621b2d7a4fc1f99fc89411bcf5166415
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-18 21:43:09 +0200

    reorder content tweaks script, apply only to pages with a container, add a class to body when the pages contain a form

--- content_tweaks.js
+++ content_tweaks.js
@@ -1,104 +1,152 @@
+// Tweaks the content of the pages to allow for better styling
+// TODO: refactor code
 load(function () {
-  // Makes a few tweaks to the content of the page
-  $("span.pagetop, span.yclinks").forEach(function (elem) {
-    toArray(elem.childNodes).forEach(function (node) {
-      if (node.nodeType === Node.TEXT_NODE) {
-        node.nodeValue = node.nodeValue.replace(/\|/g, "");
-      }
-    })
-  });
-
-  // Add p tags to the first paragraph of self posts
-  var title = document.getElementsByClassName("title")[0];
-
-  // If the first title in the page contains an A tag we're hopefully in an item page
-  // I'm not sure if there's a better way to match this.
-  if (title && title.children.length && title.children[0].nodeName === "A" && !title.children[0].getAttribute("href").match(/^\/x\?.+/)) {
-    var container = title.parentElement.parentElement.children[3].children[1];
-
-    // Don't do this when the post has no textual content
-
-    if (container.textContent.trim().length) {
-      var unwrapped = container.childNodes[0];
-      var text = unwrapped.nodeValue;
-      unwrapped.remove();
-
-      var paragraph = document.createElement("p");
-      paragraph.textContent = text;
-
-      if(container.getElementsByTagName("p").length) {
-        container.insertBefore(paragraph, container.children[0]);  
-      } else {
-        container.appendChild(paragraph);
-      }      
-    }
-  }
-
-  // Wrap the first piece of text in each comment into its own p and add a class to the upvote td
-  $("span.comment").forEach(function (elem) {
-    var paragraph = document.createElement("p");
-
-    if (elem.children.length) {
-      paragraph.appendChild(elem.children[0]);
-      elem.insertBefore(paragraph, elem.children[0]);    
-    } else {
-      paragraph.textContent = elem.textContent;
-      elem.textContent = "";
-      elem.appendChild(paragraph);
-    } 
-
-    var container = elem.parentElement.parentElement;
-
-    var index = 1;
-    if (container.children.length === 2) index = 0; // page /newcomments has two tds instead of three
-    container.children[index].classList.add("hnspecial-upvote-button");
-  });
-
-  // Add a class to the upvote buttons on poll items
-  $("td.comment").forEach(function (elem) {
-    var row = elem.parentElement;
-    row.classList.add("hnspecial-poll-row");
-    var arrow = row.children[0];
-    arrow.classList.add("hnspecial-upvote-button", "poll");
-    arrow.parentElement
-  });
-
-  // Style error pages (and ignore the rss page)
-  var body = document.body;
-  if (location.pathname !== "/rss" && (!body.children.length || body.children[0].nodeName === "PRE")) {
-    body.classList.add("error");
-
-    // Dirty hack to remove the <pre> element shown on 404 pages
-    body.innerHTML = body.textContent;
-
-    // Back link
-    var link = document.createElement("a");
-    link.setAttribute("href", location.origin);
-    link.textContent = "Back home";
-    body.appendChild(link);
-  }
-
-  // Make the logo go to the home of hacker news
-  $("img[src='y18.gif']")[0].parentElement.setAttribute("href", location.origin);
-
-  // If the third TD is empty, add all of the following TDs into it (happens in threads page)
+  // Main container (contains header and content)
   var container = $("body > center > table > tbody")[0];
-  var cell = container.children[2].children[0];
 
-  if (!cell.textContent.trim().length) {
-    // Empty the element completely
-    cell.innerHTML = "";
+  // Apply to pages with a container
+  if (container) {
+    // TD with content (after the header)
+    var content = container.children[2].children[0];
 
-    // New table to hold comments (mimics comment page structure)
-    var table = document.createElement("table");
-    var tbody = document.createElement("tbody");
-    
-    var i = 3; // First stray row is at index 3
-    while (!container.children[i].getElementsByClassName("yclinks").length) { // Stop at the footer
-      tbody.appendChild(container.children[i]);
+    // If the content TD is empty, add all of the following TDs into it (happens in threads page)
+    if (!content.textContent.trim().length) {
+      // Empty the element completely
+      content.innerHTML = "";
+
+      // New table to hold comments (mimics comment page structure)
+      var table = document.createElement("table");
+      var tbody = document.createElement("tbody");
+      
+      var i = 3; // First stray row is at index 3
+      while (!container.children[i].getElementsByClassName("yclinks").length) { // Stop at the footer
+        tbody.appendChild(container.children[i]);
+      }
+
+      table.appendChild(tbody);
+      content.appendChild(table);
     }
 
-    table.appendChild(tbody);
-    cell.appendChild(table);
+    // Add a class to the body if it's a form page
+    // Get the post title
+    var title = document.getElementsByClassName("title")[0];
+    var isCommentPage = title && title.children.length && title.children[0].nodeName === "A" && !title.children[0].getAttribute("href").match(/^\/x\?.+/);
+
+    // Get the form in the content td
+    var form = content.getElementsByTagName("form")[0];
+
+    if (form && !isCommentPage) {
+      console.log("With forms");
+
+      document.body.classList.add("hnspecial-form-page");
+      var form = document.getElementsByTagName("form")[0];
+      toArray(form.getElementsByTagName("textarea")).forEach(function (textarea) {
+        textarea.parentElement.parentElement.children[0].classList.add("hnspecial-textarea-label");
+      });
+
+      // Fix up stray text near P tags in form pages
+      toArray(form.getElementsByTagName("p")).forEach(function (paragraph) {
+        // Refactor: this code is the same as below
+        
+        var container = paragraph.parentElement;
+        console.log(container);
+        var unwrapped = container.childNodes[0];
+        var text = unwrapped.nodeValue;
+        console.log(text);
+        unwrapped.remove();
+
+        var paragraph = document.createElement("p");
+        paragraph.textContent = text;
+
+        container.insertBefore(paragraph, container.children[0]);
+      });
+    } else {
+      console.log("Without forms");
+    }
+
+    // Make the logo go to the home of hacker news
+    $("img[src='y18.gif']")[0].parentElement.setAttribute("href", location.origin);
+
+    // Remove some of the vertical bars
+    $("span.pagetop, span.yclinks").forEach(function (elem) {
+      toArray(elem.childNodes).forEach(function (node) {
+        if (node.nodeType === Node.TEXT_NODE) {
+          node.nodeValue = node.nodeValue.replace(/\|/g, "");
+        }
+      })
+    });
+
+    // If the first title in the page contains an A tag we're hopefully in an item page
+    // I'm not sure if there's a better way to match this.
+    if (isCommentPage) {
+      var textContainer = title.parentElement.parentElement.children[3].children[1];
+
+      // Only apply this when the post has textual content
+      if (textContainer.textContent.trim().length) {
+        var unwrapped = textContainer.childNodes[0];
+        var text = unwrapped.nodeValue;
+        unwrapped.remove();
+
+        var paragraph = document.createElement("p");
+        paragraph.textContent = text;
+
+        if (textContainer.getElementsByTagName("p").length) {
+          textContainer.insertBefore(paragraph, textContainer.children[0]);  
+        } else {
+          textContainer.appendChild(paragraph);
+        }      
+      }
+    }
+
+    // Wrap the first piece of text in each comment into its own p and add a class to the upvote td
+    $("span.comment").forEach(function (elem) {
+      var paragraph = document.createElement("p");
+
+      if (elem.children.length) {
+        paragraph.appendChild(elem.children[0]);
+        elem.insertBefore(paragraph, elem.children[0]);    
+      } else {
+        paragraph.textContent = elem.textContent;
+        elem.textContent = "";
+        elem.appendChild(paragraph);
+      } 
+
+      var container = elem.parentElement.parentElement;
+
+      var index = 1;
+      if (container.children.length === 2) index = 0; // page /newcomments has two tds instead of three
+      container.children[index].classList.add("hnspecial-upvote-button");
+    });
+
+    // Add a class to the upvote buttons on poll items
+    $("td.comment").forEach(function (elem) {
+      var row = elem.parentElement;
+      row.classList.add("hnspecial-poll-row");
+      var arrow = row.children[0];
+      arrow.classList.add("hnspecial-upvote-button", "poll");
+      arrow.parentElement
+    });
+  } else {
+    // The page has no container. It's either the login page or an error page
+
+    // Style error pages (and ignore the rss page)
+    var body = document.body;
+    if (location.pathname !== "/rss" && (!body.children.length || body.children[0].nodeName === "PRE")) {
+      body.classList.add("error");
+
+      // Dirty hack to remove the <pre> element shown on 404 pages
+      body.innerHTML = body.textContent;
+
+      // Back link
+      var link = document.createElement("a");
+      link.setAttribute("href", location.origin);
+      link.textContent = "Back home";
+      body.appendChild(link);
+    }
   }
+
+
+
+
+  
 });

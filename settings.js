5d57e61b9c440923e362a933a8a3d6f87ef3a77e
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-19 01:02:12 +0200

    super stellar animated 100% CSS gear menu !!!!! !!

--- settings.js
+++ settings.js
@@ -10,6 +10,25 @@ function createMenu() {
 
   var button = document.createElement("div");
   button.classList.add("hnspecial-settings-button");
+  
+  var checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.classList.add("hnspecial-settings-button-checkbox");
+  button.appendChild(checkbox);
+
+  var image = document.createElement("img");
+  image.setAttribute("src", chrome.extension.getURL("gear.svg"));
+  button.appendChild(image);
+
+  var menu = document.createElement("div");
+  menu.classList.add("hnspecial-settings-menu-container");
+  
+  var inner = document.createElement("div");
+  inner.classList.add("hnspecial-settings-menu-inner");
+  inner.textContent = "I'm a super cool menu";
+
+  menu.appendChild(inner);
+  button.appendChild(menu);
 
   container.insertBefore(button, container.children[0]);
 }

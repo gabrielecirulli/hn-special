290076944018e0e634d4caf966460ef854173fef
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-20 18:23:33 +0200

    move clear_styles code into content_tweaks

--- content_tweaks.js
+++ content_tweaks.js
@@ -1,6 +1,28 @@
 // Tweaks the content of the pages to allow for better styling
 // TODO: refactor code
 HNSpecial.settings.loadConditional("visual_theme", function () {
+  // Removes the original HN CSS to avoid conflicts with the CSS added by the extension 
+  _.$("link[rel=stylesheet], style").forEach(function (elem) {
+      elem.remove();
+  });
+   
+  // Removes all styling attributes
+  _.$("body, table, tr, td, span, p, font, div").forEach(function (elem) {
+      var attrs = elem.attributes;
+      var names = [];
+
+      // This is contrived because .length is changed, messing up the loop
+      for (var i = 0; i < attrs.length; i++) {
+        var attr = attrs[i].name;
+        if (["colspan", "class", "id"].indexOf(attr) !== -1) continue; 
+        names.push(attr);
+      }
+
+      names.forEach(function (attr) {
+        elem.removeAttribute(attr);
+      });
+  });
+  
   // Main container (contains header and content)
   var body = document.body;
   body.classList.add("hnspecial-theme");

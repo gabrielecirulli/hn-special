fa1f40ab22a87cd8c84dfdf78f23aa70414a7d13
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-20 18:21:57 +0200

    complete open_links_in_new_tabs module

--- open_links_in_new_tabs.js
+++ open_links_in_new_tabs.js
@@ -1,10 +1,18 @@
 HNSpecial.settings.loadConditional("open_links_in_new_tabs", function () {
-  _.toArray(document.getElementsByTagName("a")).forEach(function (link) {
-    var parent = link.parentElement;
-    if (parent.classList.contains("title") && parent.parentElement.childElementCount === 3) {
-      console.log(link.textContent);
-      link.setAttribute("target", "_blank");  
-    }    
-  });
+  function tweakLinks() {
+    _.toArray(document.getElementsByTagName("a")).forEach(function (link) {
+      if (link.parentElement.classList.contains("title") && !link.getAttribute("href").match(/^\/x\S+/)) {
+        console.log(link.textContent);
+        link.setAttribute("target", "_blank");  
+      }    
+    });  
+  }
+  
+  // Run it
+  tweakLinks();
 
+  // Used by other modules to notify the presence of new links
+  HNSpecial.notifyLinks = function () {
+    tweakLinks();
+  };
 });

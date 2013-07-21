d08896fd90e0676aec2d1771ff045f761eb24112
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-21 20:41:05 +0200

    fix some domains not matching

--- accurate_domain_names.js
+++ accurate_domain_names.js
@@ -5,7 +5,7 @@ HNSpecial.settings.registerModule("accurate_domain_names", function () {
     titles.forEach(function (title) {
       if (!title.getAttribute("data-hnspecial-accurate") && title.childElementCount === 2 && title.children[1].classList.contains("comhead")) {
         // Removes http/https, matches the domain name excluding www
-        var url = title.children[0].getAttribute("href").replace(/http(?:s)?:\/\/(?:www.)?([a-z0-9.-]+)(?:\/\S*)/i, "$1").toLowerCase();
+        var url = title.children[0].getAttribute("href").replace(/http(?:s)?:\/\/(?:www.)?([a-z0-9.-]+)(?:\/\S*)?/i, "$1").toLowerCase();
         var domain = title.children[1];
         domain.textContent = " (" + url + ") ";
         title.setAttribute("data-hnspecial-accurate", "true");

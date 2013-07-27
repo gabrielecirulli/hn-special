821c67ce344f23b6b02a55595f3b1b004db28934
Gabriele Cirulli <gabrycir@gmail.com>
2013-07-27 19:51:01 +0200

    [#3] Only flash links when clicked with the left button

--- highlight_links_when_returning.js
+++ highlight_links_when_returning.js
@@ -5,9 +5,11 @@ HNSpecial.settings.registerModule("highlight_links_when_returning", function () 
       var parent = link.parentElement.parentElement;
       if (_.isTitleLink(link) && !link.getAttribute("data-hnspecial-flash-effect")) {
         link.setAttribute("data-hnspecial-flash-effect", "true");
-        link.addEventListener("click", function () {
-          parent.classList.remove("hnspecial-theme-link-flash");
-          parent.classList.add("hnspecial-theme-link-pre-flash");
+        link.addEventListener("click", function (e) {
+          if (e.button === 0) { // Ignore all clicks except left click
+            parent.classList.remove("hnspecial-theme-link-flash");
+            parent.classList.add("hnspecial-theme-link-pre-flash");  
+          }          
         });
       }    
     });  
@@ -21,7 +23,6 @@ HNSpecial.settings.registerModule("highlight_links_when_returning", function () 
   // Flash the links when the window is focused
   document.addEventListener("webkitvisibilitychange", function (e) {
     if (!this.webkitHidden) {
-
       var link = document.getElementsByClassName("hnspecial-theme-link-pre-flash")[0];
       if (link) {
         link.classList.remove("hnspecial-theme-link-pre-flash");

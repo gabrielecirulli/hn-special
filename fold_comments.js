ed8d2803602880d23c489bc821a3d8701b284110
Gabriele Cirulli <gabrycir@gmail.com>
2013-08-17 18:23:34 +0200

    comment folding

--- fold_comments.js
+++ fold_comments.js
@@ -3,7 +3,7 @@ HNSpecial.settings.registerModule("fold_comments", function () {
 
   if (_.isCommentPage()) {
     _.toArray(document.getElementsByClassName("default")).forEach(function (comment) {
-      var row = comment.parentElement;
+      var row = comment.parentElement.parentElement.parentElement.parentElement.parentElement; // Least horrible way to get to the comment row
       var depth = getCommentDepth(row);
 
       var comhead = comment.getElementsByClassName("comhead")[0];
@@ -16,13 +16,22 @@ HNSpecial.settings.registerModule("fold_comments", function () {
       comhead.appendChild(button);
 
       button.addEventListener("click", function () {
-        
+        var current = row;
+
+        while (current = current.nextSibling) {
+          if (getCommentDepth(current) <= depth) break;
+
+          current.setAttribute("style", "display: none");
+        }
+
+        // Fold the current comment
+        comment.classList.add("hnspecial-folded-comment");
       });
     });
   }
 
   function getCommentDepth(comment) {
-    var spacer = comment.firstChild.firstChild;
+    var spacer = comment.getElementsByTagName("img")[0];
     return parseInt(spacer.getAttribute("width"), 10) / baseWidth;
   }
 });

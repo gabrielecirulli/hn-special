ccfddde1aaf53fbda1db51047a4b3b36a5679ada
Gabriele Cirulli <gabrycir@gmail.com>
2013-08-17 17:02:40 +0200

    basic comment folding button and depth detection

--- fold_comments.js
+++ fold_comments.js
@@ -1,3 +1,28 @@
 HNSpecial.settings.registerModule("fold_comments", function () {
-  console.log("Fold comments");
+  var baseWidth = 40;
+
+  if (_.isCommentPage()) {
+    _.toArray(document.getElementsByClassName("default")).forEach(function (comment) {
+      var row = comment.parentElement;
+      var depth = getCommentDepth(row);
+
+      var comhead = comment.getElementsByClassName("comhead")[0];
+      comhead.appendChild(document.createTextNode(" | "));
+
+      var button = _.createElement("button", {
+        content: "fold",
+        classes: ["hnspecial-fold-comment-button"]
+      });
+      comhead.appendChild(button);
+
+      button.addEventListener("click", function () {
+        
+      });
+    });
+  }
+
+  function getCommentDepth(comment) {
+    var spacer = comment.firstChild.firstChild;
+    return parseInt(spacer.getAttribute("width"), 10) / baseWidth;
+  }
 });

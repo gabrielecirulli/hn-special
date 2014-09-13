6589ef3eae51abd8a2e4134268e4f13bfba2a194
Colden Cullen <ColdenCullen@gmail.com>
2014-09-13 14:16:42 -0400

    Added more browser abstraction things

--- index.js
+++ index.js
@@ -5,29 +5,38 @@ console.error( "I actually loaded, I promise." );
 
 function notDataUrl( name )
 {
-    return self.data.url( name ).replace( "/data/", "/" );
+  return self.data.url( name ).replace( "/data/", "/" );
+}
+
+function notDataRead( name )
+{
+  return self.data.load( "../" + name );
 }
 
 pageMod.PageMod({
     include: "*.ycombinator.com",
     attachTo: [ "top" ],
     onAttach: function( things ) {
-        console.error( "Attached!" );
+      console.error( "Attached!" );
     },
     contentStyleFile: [ notDataUrl( "lib/extras/hn_theme.css" ) ],
     contentScriptFile: [
-        notDataUrl( "lib/tools/utility.js" ),
-        notDataUrl( "lib/settings.js" ),
-        notDataUrl( "lib/modules/visual_theme.js" ),
-        notDataUrl( "lib/modules/high_contrast.js" ),
-        notDataUrl( "lib/modules/gray_visited_links.js" ),
-        notDataUrl( "lib/modules/open_links_in_new_tabs.js" ),
-        notDataUrl( "lib/modules/highlight_links_when_returning.js" ),
-        notDataUrl( "lib/modules/infinite_scrolling.js" ),
-        notDataUrl( "lib/modules/accurate_domain_names.js" ),
-        notDataUrl( "lib/modules/mark_as_read.js" ),
-        notDataUrl( "lib/modules/fold_comments.js" ),
-        notDataUrl( "lib/modules/sticky_header.js" ),
-        notDataUrl( "lib/modules/user_tooltips.js" )
+      notDataUrl( "lib/tools/utility.js" ),
+      notDataUrl( "lib/settings.js" ),
+      notDataUrl( "lib/modules/visual_theme.js" ),
+      notDataUrl( "lib/modules/high_contrast.js" ),
+      notDataUrl( "lib/modules/gray_visited_links.js" ),
+      notDataUrl( "lib/modules/open_links_in_new_tabs.js" ),
+      notDataUrl( "lib/modules/highlight_links_when_returning.js" ),
+      notDataUrl( "lib/modules/infinite_scrolling.js" ),
+      notDataUrl( "lib/modules/accurate_domain_names.js" ),
+      notDataUrl( "lib/modules/mark_as_read.js" ),
+      notDataUrl( "lib/modules/fold_comments.js" ),
+      notDataUrl( "lib/modules/sticky_header.js" ),
+      notDataUrl( "lib/modules/user_tooltips.js" )
     ],
+    contentScriptOptions: {
+      urlBase: notDataUrl( "" ),
+      defaultOptions: notDataRead( "lib/defaults.json" )
+    }
 });
